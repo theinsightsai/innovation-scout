322d8b515c1c16d27fe6cf286961bd691fb63f96
Ajit <ajit@gmail.com>
2025-01-28 17:05:59 +0530

    first commit

---
+++ components/index.js
@@ -0,0 +1,3 @@
+export { default as LandingPage } from "./landing";
+export { default as Login } from "./auth/Login";
+export { default as Navbar } from "./navbar";
---
+++ constants/index.js
@@ -0,0 +1,4 @@
+export const IMAGES = {
+  LOGO: "/assets/logo.webp",
+  BANNER: "/assets/main-banner-bg.webp",
+};
