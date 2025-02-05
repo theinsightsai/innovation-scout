9cc96058673fd4665a16518fab7a7b750d469d9f
Ajit <ajit@gmail.com>
2025-02-05 19:05:50 +0530

    logs section page UI

--- app/logs/page.jsx
+++ app/logs/page.jsx
@@ -1,7 +1,13 @@
-"use client"
+"use client";
+import { Fragment } from "react";
 import withLayout from "@/components/hoc/withLayout";
+import { PageHeader } from "@/components";
 
 const Logs = () => {
-  return <>Logs</>;
+  return (
+    <Fragment>
+      <PageHeader text="Logs" buttonText={""} onButtonClick={""} icon={""} />
+    </Fragment>
+  );
 };
 export default withLayout(Logs);
